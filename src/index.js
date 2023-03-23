3e0ee2f6307d17fb1b75ff0e3675c5bef34f9a02
Bibbleboop <nicjah@hotmail.com>
2023-03-23 17:56:36 +0000

    remove strict modified

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React>
     <App />
-  </React.StrictMode>
+  </React>
 );
 
 // If you want to start measuring performance in your app, pass a function
