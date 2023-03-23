72af1beaa28bf99ce144de5851130a1f721f7f7e
Bibbleboop <nicjah@hotmail.com>
2023-03-23 18:25:48 +0000

    remove restrict mode

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React>
+  // <React>
     <App />
-  </React>
+  // </React>
 );
 
 // If you want to start measuring performance in your app, pass a function
