dc8e2445a742bab51ea5075f380239cc5c70b835
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-03-26 00:55:56 +0530

    added search icon inside button

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -41,12 +41,12 @@ const search = () => {
           onKeyDown={handleKeyDown}
         />
         <button
-          className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
+          className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
           type="button"
           onClick={search}
           
         >
-
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </button>
       </div>
       <div className="flex flex-wrap justify-center">
