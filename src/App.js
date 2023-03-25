a68988ddf5099a815d6a4d5ec1f6d6c602ea4485
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-03-26 00:00:10 +0530

    added test footer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import SearchBar from './SearchBar';
 import ReactFooter from '../src/Components/footer/footer'
+import React from 'react';
+import SearchBar from './SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold centre">Search For Events, Clubs, Raves & much more!</h1>
+      <h1 className="text-3xl text-center text-cyan-600 m-6">Search Events, Clubs, Raves & much more!</h1>
       <div className="container mx-auto my-4">
         <SearchBar />
       </div>
-      <ReactFooter />
+    <ReactFooter />
     </div>
   );
 }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -10,7 +10,8 @@ const search = () => {
   const search = new SerpApi.GoogleSearch("99e862960da1f6c3c8cfcc8f6ba4f35968cee4846f4411a8a8e75b8e697c3d9e");
   const params = {
         q: query,
-    engine: "google_events"
+    engine: "google_events",
+    num: "20"
   };
   search.json(params, (data) => {
     setResults(data.events_results);
@@ -43,8 +44,9 @@ const search = () => {
           className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
           type="button"
           onClick={search}
+          
         >
-          Search
+
         </button>
       </div>
       <div className="flex flex-wrap justify-center">
@@ -52,8 +54,8 @@ const search = () => {
           <Card
             key={result.cache_id}
             title={result.title}
-            description={result.snippet}
-            image={result.thumbnail}
+            date={result.date}
+            image={result.image}
             link={result.link}
           />
         ))}
