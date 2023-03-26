6ff01c682d5f3b7eefa27f99b2fd4031d218b688
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-03-26 18:23:45 +0530

    changed file structure and removed errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-=======
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
+  content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
->>>>>>> 6afe2aff9357c4fad43048c776a105539da91460
   plugins: [],
 }
