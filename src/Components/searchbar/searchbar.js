094ee93feecea08a8c20288d3a7c7577a0b62b64
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-03-28 03:15:58 +0530

    added btns that filter events as per time, updated cards styles

--- src/Components/searchbar/searchbar.js
+++ src/Components/searchbar/searchbar.js
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 import SerpApi from 'google-search-results-nodejs';
 import Card from '../cards/Card';
+import SearchButtons from '../searchbuttons/SearchButtons';
 
-function SearchBar() {
+function SearchBar(props) {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
+  const [filter, setFilter] = useState('');
 
-const search = () => {
-  const search = new SerpApi.GoogleSearch("c454b774640a879beb6a843015825e7d8c4e98701f51969d6dd8e5906d392064");
-  const params = {
-        q: query,
-    engine: "google_events",
-    num: "20"
+
+  const search = () => {
+    const search = new SerpApi.GoogleSearch("c454b774640a879beb6a843015825e7d8c4e98701f51969d6dd8e5906d392064");
+    const params = {
+      q: query,
+      engine: "google_events",
+      htichips: filter
+    };
+    search.json(params, (data) => {
+      setResults(data.events_results);
+    });
   };
-  search.json(params, (data) => {
-    setResults(data.events_results);
-  });
-};
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
@@ -26,6 +29,11 @@ const search = () => {
     if (event.key === 'Enter') {
       search();
     }
+  };
+
+  const handleButtonClick = (filter) => {
+    setFilter(filter);
+    search();
   };
 
   return (
@@ -44,20 +52,14 @@ const search = () => {
           className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
           type="button"
           onClick={search}
-          
         >
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </button>
       </div>
+      <SearchButtons setFilter={handleButtonClick} />
       <div className="flex flex-wrap justify-center">
         {Array.isArray(results) && results.map((result) => (
-          <Card
-            key={result.cache_id}
-            title={result.title}
-            date={result.date}
-            image={result.image}
-            link={result.link}
-          />
+          <Card key={result.id} {...result} />
         ))}
       </div>
     </div>
