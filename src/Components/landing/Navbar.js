4c4b4def60931feac571f8c863657a49f3a60af5
Chaitanya Varma <gchaitanya.369@gmail.com>
2023-03-30 17:15:15 +0530

    Revert "updated everything"

--- src/Components/landing/Navbar.js
+++ src/Components/landing/Navbar.js
@@ -1,114 +1,112 @@
-import React, { useState } from "react";
-import { Link, animateScroll as scroll } from "react-scroll";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import React, { useState } from 'react';
+import { Link, animateScroll as scroll } from 'react-scroll';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
-
-  const handleClick = () => {
+    const [nav, setNav] = useState(false)
+     const handleClick = () => {
     scroll.scrollTo(1000, {
       duration: 500,
       delay: 100,
-      smooth: "easeInOutQuart",
+      smooth: 'easeInOutQuart'
     });
-  };
+  }
 
-  const handleClose = () => setNav(false);
+    const handleClose =()=> setNav(!nav)
 
-  return (
-    <div>
-      <div className="w-screen h-[80px] z-10 bg-cyan-900 fixed drop-shadow-lg">
-        <div className="px-2 flex justify-between items-center w-full h-full">
-          <div className="flex items-center">
-            <img
-              src={require("../logodesign/logo.png")}
-              className="w-20 h-20"
-              alt="logo"
-            />
 
-            <ul className="hidden md:flex text-slate-200  cursor-pointer">
-              <li>
-                <Link to="home" smooth={true} duration={500}>
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="about" smooth={true} offset={-200} duration={500}>
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="fav"
-                  smooth={true}
-                  offset={-100}
-                  duration={500}
-                >
-                  Favourites
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="contact-us"
-                  smooth={true}
-                  offset={-100}
-                  duration={500}
-                >
-                  Contact Us
-                </Link>
-              </li>
-            </ul>
+    return (
+      <div>
+        <div className="w-screen h-[80px] z-10 bg-cyan-900 fixed drop-shadow-lg">
+          <div className="px-2 flex justify-between items-center w-full h-full">
+            <div className="flex items-center">
+              <h1 className="text-3xl font-bold mr-4 sm:text-4xl text-slate-200">
+                EAT RAVE LOVE.
+              </h1>
+              <ul className="hidden md:flex text-slate-200  cursor-pointer">
+                <li>
+                  <Link to="home" smooth={true} duration={500}>
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link to="about" smooth={true} offset={-200} duration={500}>
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="contact-us"
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                  >
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="hidden md:flex pr-4">
+              <button className="px-8 py-3">Favourites</button>
+            </div>
+            <div className="md:hidden" onClick={handleClick}>
+              {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
+            </div>
           </div>
-          <div className="hidden md:flex pr-4">
-          </div>
-          <div className="md:hidden" onClick={handleClick}>
-            {!nav ? <MenuIcon className="w-5" /> : <XIcon className="w-5" />}
-          </div>
+
+          <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
+            <li className="border-b-2 border-zinc-300 w-full">
+              <Link
+                onClick={handleClose}
+                to="home"
+                smooth={true}
+                duration={500}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="border-b-2 border-zinc-300 w-full">
+              <Link
+                onClick={handleClose}
+                to="about"
+                smooth={true}
+                offset={-200}
+                duration={500}
+              >
+                About
+              </Link>
+            </li>
+            <li className="border-b-2 border-zinc-300 w-full">
+              <Link
+                onClick={handleClose}
+                to="footer"
+                smooth={true}
+                offset={-100}
+                duration={500}
+              >
+                Footer
+              </Link>
+            </li>
+            <li className="border-b-2 border-zinc-300 w-full">
+              <Link
+                onClick={handleClose}
+                to="contactUs"
+                smooth={true}
+                offset={-50}
+                duration={500}
+              >
+                Contact Us
+              </Link>
+            </li>
+            <div className="flex flex-col my-4">
+              <button className="px-8 py-3">Favourites</button>
+            </div>
+          </ul>
         </div>
-
-        <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
-          <li className="border-b-2 border-zinc-300 w-full">
-            <Link onClick={handleClose} to="home" smooth={true} duration={500}>
-              Home
-            </Link>
-          </li>
-          <li className="border-b-2 border-zinc-300 w-full">
-            <Link
-              onClick={handleClose}
-              to="about"
-              smooth={true}
-              offset={-200}
-              duration={500}
-            >
-              About
-            </Link>
-          </li>
-          <li className="border-b-2 border-zinc-300 w-full">
-            <Link
-              onClick={handleClose}
-              to="footer"
-              smooth={true}
-              offset={-100}
-              duration={500}
-            >
-              Footer
-            </Link>
-          </li>
-          <li className="border-b-2 border-zinc-300 w-full">
-            <Link
-              onClick={handleClose}
-              to="contactUs"
-              smooth={true}
-              offset={-50}
-              duration={500}
-            >
-              Contact Us
-            </Link>
-          </li>
-        </ul>
       </div>
-    </div>
-  );
+    );
 };
+
 
 export default Navbar;
--- src/Components/searchbuttons/SearchButtons.js
+++ src/Components/searchbuttons/SearchButtons.js
@@ -13,49 +13,49 @@ const SearchButtons = ({ setFilter }) => {
     
     <div className="m-6 space-x-4">
   <button
-    className={`flex-shrink-0 bg-cyan-950 hover:bg-slate-700 text-sm text-white py-1 px-2 rounded-xl ${selectedButton === 'date:today' ? 'selected bg-slate-500' : ''}`}
+    className={`flex-shrink-0 bg-blue-500 hover:bg-red-700 text-sm text-white py-1 px-2 rounded-xl ${selectedButton === 'date:today' ? 'selected bg-red-500' : ''}`}
     type="button"
     onClick={() => handleButtonClick('date:today')}
   >
     Today's Events
   </button>
   <button
-    className={`flex-shrink-0 bg-cyan-950 hover:bg-slate-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'date:tomorrow' ? 'selected bg-slate-500' : ''}`}
+    className={`flex-shrink-0 bg-blue-500 hover:bg-red-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'date:tomorrow' ? 'selected bg-red-500' : ''}`}
     type="button"
     onClick={() => handleButtonClick('date:tomorrow')}
   >
     Tomorrow's Events
   </button>
   <button
-    className={`flex-shrink-0 bg-cyan-950 hover:bg-slate-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'date:week' ? 'selected bg-slate-500' : ''}`}
+    className={`flex-shrink-0 bg-blue-500 hover:bg-red-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'date:week' ? 'selected bg-red-500' : ''}`}
     type="button"
     onClick={() => handleButtonClick('date:week')}
   >
     This Week's Events
       </button>
       <button
-    className={`flex-shrink-0 bg-cyan-950 hover:bg-slate-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'date:next_week' ? 'selected bg-slate-500' : ''}`}
+    className={`flex-shrink-0 bg-blue-500 hover:bg-red-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'date:next_week' ? 'selected bg-red-500' : ''}`}
         type="button"
         onClick={() => handleButtonClick('date:next_week')}
       >
         Next Week's Events
       </button>
       <button
-    className={`flex-shrink-0 bg-cyan-950 hover:bg-slate-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'date:month' ? 'selected bg-slate-500' : ''}`}
+    className={`flex-shrink-0 bg-blue-500 hover:bg-red-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'date:month' ? 'selected bg-red-500' : ''}`}
         type="button"
         onClick={() => handleButtonClick('date:month')}
       >
         This Month's Events
       </button>
       <button
-    className={`flex-shrink-0 bg-cyan-950 hover:bg-slate-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'date:next_month' ? 'selected bg-slate-500' : ''}`}
+    className={`flex-shrink-0 bg-blue-500 hover:bg-red-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'date:next_month' ? 'selected bg-red-500' : ''}`}
         type="button"
         onClick={() => handleButtonClick('date:next_month')}
       >
         Next Month's Events
       </button>
       <button
-    className={`flex-shrink-0 bg-cyan-950 hover:bg-slate-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'event_type:Virtual-Event' ? 'selected bg-slate-500' : ''}`}
+    className={`flex-shrink-0 bg-blue-500 hover:bg-red-700 text-sm text-white py-1 px-2 rounded ${selectedButton === 'event_type:Virtual-Event' ? 'selected bg-red-500' : ''}`}
         type="button"
         onClick={() => handleButtonClick('event_type:Virtual-Event')}
       >
